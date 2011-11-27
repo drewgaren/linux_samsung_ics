640ad95b4deece8edd8d1ddd307537be6b43a597
Shankar <mathkid95@gmail.com>
2011-11-26 21:20:34 -0600

    This commit has all of the Matr1x kernel code for gingerbread and ICS patches up to the "fix usb tethering" commit from Kwiboo

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 14 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#14 PREEMPT Sat Nov 26 19:55:32 CST 2011"
+#define LINUX_COMPILE_TIME "19:55:32"
+#define LINUX_COMPILE_BY "shankar"
+#define LINUX_COMPILE_HOST "Matr1x.local"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
